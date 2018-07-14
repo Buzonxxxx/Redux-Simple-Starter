47ae9488edea14b328033ef93e1daa88202d12e1
louis.liao <buzonliao@gmail.com>
2018-07-14 13:09:46 +0800

    finish Youtuve search project

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -11,15 +11,20 @@ class searchBar extends Component{
   constructor(props) {
     super(props)
 
-    this.state = {term: 'input'}
+    this.state = {term: ''}
   } 
   
+  onInputChange(term) {
+    this.setState({term})
+    this.props.onSearchTermChange(term)
+  }
+
   render() {
     return(
-    <div>
+    <div className="search-bar">
       <input
       value = {this.state.term} 
-      onChange = {event => this.setState({ term: event.target.value })} />
+      onChange = {event => this.onInputChange(event.target.value)} />
     </div>
     )
   }
